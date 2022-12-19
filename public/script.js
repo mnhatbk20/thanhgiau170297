f7b41bc887d339c38ca6e58d778aef369461d61d
mnhatbk20 <mnhatbk20@gmail.com>
2022-12-19 23:30:58 +0700

    change UI

--- public/script.js
+++ public/script.js
@@ -1,4 +1,6 @@
 /*kalmanjs, Wouter Bulten, MIT, https://github.com/wouterbulten/kalmanjs */
+
+
 var KalmanFilter = function () { "use strict"; function s(t, i) { for (var e = 0; e < i.length; e++) { var s = i[e]; s.enumerable = s.enumerable || !1, s.configurable = !0, "value" in s && (s.writable = !0), Object.defineProperty(t, s.key, s) } } return function () { function v() { var t = 0 < arguments.length && void 0 !== arguments[0] ? arguments[0] : {}, i = t.R, e = void 0 === i ? 1 : i, s = t.Q, n = void 0 === s ? 1 : s, r = t.A, h = void 0 === r ? 1 : r, a = t.B, o = void 0 === a ? 0 : a, u = t.C, c = void 0 === u ? 1 : u; !function (t, i) { if (!(t instanceof i)) throw new TypeError("Cannot call a class as a function") }(this, v), this.R = e, this.Q = n, this.A = h, this.C = c, this.B = o, this.cov = NaN, this.x = NaN } var t, i, e; return t = v, (i = [{ key: "filter", value: function (t) { var i = 1 < arguments.length && void 0 !== arguments[1] ? arguments[1] : 0; if (isNaN(this.x)) this.x = 1 / this.C * t, this.cov = 1 / this.C * this.Q * (1 / this.C); else { var e = this.predict(i), s = this.uncertainty(), n = s * this.C * (1 / (this.C * s * this.C + this.Q)); this.x = e + n * (t - this.C * e), this.cov = s - n * this.C * s } return this.x } }, { key: "predict", value: function () { var t = 0 < arguments.length && void 0 !== arguments[0] ? arguments[0] : 0; return this.A * this.x + this.B * t } }, { key: "uncertainty", value: function () { return this.A * this.cov * this.A + this.R } }, { key: "lastMeasurement", value: function () { return this.x } }, { key: "setMeasurementNoise", value: function (t) { this.Q = t } }, { key: "setProcessNoise", value: function (t) { this.R = t } }]) && s(t.prototype, i), e && s(t, e), v }() }();
 
 var delayESP = 1
@@ -347,7 +349,16 @@ function Init() {
 
 	});
 
-	$("#restore").click(Restore);
+	$("#move-up").click(function () {
+		db.ref().update({ 'motor2/steps': 2500 })
+		db.ref().update({ 'motor2/dir': 0 })
+		db.ref().update({ 'motor2/run': 1 })
+	});
+	$("#move-down").click(function () {
+		db.ref().update({ 'motor2/steps': 2500 })
+		db.ref().update({ 'motor2/dir': 1 })
+		db.ref().update({ 'motor2/run': 1 })
+	});
 
 
 
@@ -405,6 +416,83 @@ doneEvent.on('value', (snapshot) => {
 
 });
 
+
+function drawFull(startTime,endTime){
+	
+	tempdataY_segment = []
+	tempdataX_segment = []
+
+	dataY1.forEach(function (item, index) {
+		if ((index >= startTime) && (index <= endTime)) {
+			tempdataY_segment.push(item - dataY1[startTime])
+		}
+	})
+	tempdataY_segment.forEach(function (item, index) {
+		tempdataX_segment.push(index * delayESP)
+	})
+
+	CalculateData_V(tempdataY_segment);
+	// CalculateData_A(dataY2);
+	CalculateData_A(tempdataY_segment);
+
+
+	if (chart1.data !== undefined) {
+		while (chart1.data.length > 0) {
+			Plotly.deleteTraces(chart1, [0]);
+		}
+	}
+	if (chart2.data !== undefined) {
+		while (chart2.data.length > 0) {
+			Plotly.deleteTraces(chart2, [0]);
+		}
+	}
+	if (chart3.data !== undefined) {
+		while (chart3.data.length > 0) {
+			Plotly.deleteTraces(chart3, [0]);
+		}
+	}
+
+	Draw([0, (tempdataY_segment.length - 1) * delayESP], [0, 1000], tempdataX_segment, tempdataY_segment, '', 'Thời gian (ms)', 'Tọa độ (mm)', 'chart1')
+	Draw([0, (dataY2.length - 1) * delayESP], [0, 2], dataX2, dataY2, '', 'Thời gian (ms)', 'Vận tốc (m/s)', 'chart2')
+
+	Draw([0, (dataY3.length - 1) * delayESP], [-5, 5], dataX3, dataY3, '', 'Thời gian (ms)', 'Gia tốc (m/s^2', 'chart3')
+
+
+	var dataTable = []
+	maxLength = tempdataY_segment.length
+	dataTable.push(DataRow(function (i) { return (i * delayESP).toString() }, "Thời gian (ms)", stepPoint))
+	dataTable.push(DataRow(function (i) { return round(tempdataY_segment[i], 2) }, "Tọa độ (mm)", stepPoint))
+	dataTable.push(DataRow(function (i) { return round(dataY2[i], 2) }, "Vận tốc (m/s)", stepPoint))
+
+	createTable(dataTable, 'table');
+	ShowEq()
+
+	expectedX = []
+	expectedY = []
+	for (let t = 0; t < tempdataY_segment.length; t++) {
+		expectedY.push(cof[0] * t * t / 1000 + cof[1] * t)
+		expectedX.push(t)
+	}
+	Draw([0, (expectedY.length - 1) * delayESP], [0, 1000], expectedX, expectedY, '', 'Thời gian (ms)', 'Tọa độ (mm)', 'chart1')
+	// $('#chart1Main').hide()
+
+	Relayout()
+
+	$(".nav-item").each(function (index) {
+
+		$(this).click(function (e) {
+			e.preventDefault();
+			$('.detail-info-item').addClass('hide')
+			$('.detail-info-item').eq(index).removeClass('hide')
+			$(".nav-item").removeClass('active')
+			$(".nav-item").eq(index).addClass('active')
+
+			Relayout()
+
+		});
+	});
+}
+
 function getDone() {
 	$("#start").hide()
 	$("#reset").show();
@@ -423,88 +511,21 @@ function getDone() {
 		$('.select-time-item input[name=endTime]').attr('value', `${maxLength}`)
 
 
+
 		CalculateData_S(data);
 		Draw([0, (maxLength - 1) * delayESP], [0, 1000], dataX1, dataY1, '', 'Thời gian (ms)', 'Tọa độ (mm)', 'chart1')
+
+		var startTime = parseInt($('#startTime').val())
+		var endTime = parseInt($('#endTime').val())
+
+		drawFull(startTime,endTime)
 
 
 		$('#submit').click(function (e) {
 			e.preventDefault();
 			var startTime = parseInt($('#startTime').val())
 			var endTime = parseInt($('#endTime').val())
-
-
-			tempdataY_segment = []
-			tempdataX_segment = []
-
-			dataY1.forEach(function (item, index) {
-				if ((index >= startTime) && (index <= endTime)) {
-					tempdataY_segment.push(item - dataY1[startTime])
-				}
-			})
-			tempdataY_segment.forEach(function (item, index) {
-				tempdataX_segment.push(index * delayESP)
-			})
-
-			CalculateData_V(tempdataY_segment);
-			// CalculateData_A(dataY2);
-			CalculateData_A(tempdataY_segment);
-
-
-			if (chart1.data !== undefined) {
-				while (chart1.data.length > 0) {
-					Plotly.deleteTraces(chart1, [0]);
-				}
-			}
-			if (chart2.data !== undefined) {
-				while (chart2.data.length > 0) {
-					Plotly.deleteTraces(chart2, [0]);
-				}
-			}
-			if (chart3.data !== undefined) {
-				while (chart3.data.length > 0) {
-					Plotly.deleteTraces(chart3, [0]);
-				}
-			}
-
-			Draw([0, (tempdataY_segment.length - 1) * delayESP], [0, 1000], tempdataX_segment, tempdataY_segment, '', 'Thời gian (ms)', 'Tọa độ (mm)', 'chart1')
-			Draw([0, (dataY2.length - 1) * delayESP], [0, 2], dataX2, dataY2, '', 'Thời gian (ms)', 'Vận tốc (m/s)', 'chart2')
-
-			Draw([0, (dataY3.length - 1) * delayESP], [-5, 5], dataX3, dataY3, '', 'Thời gian (ms)', 'Gia tốc (m/s^2', 'chart3')
-
-
-			var dataTable = []
-			maxLength = tempdataY_segment.length
-			dataTable.push(DataRow(function (i) { return (i * delayESP).toString() }, "Thời gian (ms)", stepPoint))
-			dataTable.push(DataRow(function (i) { return round(tempdataY_segment[i], 2) }, "Tọa độ (mm)", stepPoint))
-			dataTable.push(DataRow(function (i) { return round(dataY2[i], 2) }, "Vận tốc (m/s)", stepPoint))
-
-			createTable(dataTable, 'table');
-			ShowEq()
-
-			expectedX = []
-			expectedY = []
-			for (let t = 0; t < tempdataY_segment.length; t++) {
-				expectedY.push(cof[0] * t * t / 1000 + cof[1] * t)
-				expectedX.push(t)
-			}
-			Draw([0, (expectedY.length - 1) * delayESP], [0, 1000], expectedX, expectedY, '', 'Thời gian (ms)', 'Tọa độ (mm)', 'chart1')
-			// $('#chart1Main').hide()
-
-			Relayout()
-
-			$(".nav-item").each(function (index) {
-
-				$(this).click(function (e) {
-					e.preventDefault();
-					$('.detail-info-item').addClass('hide')
-					$('.detail-info-item').eq(index).removeClass('hide')
-					$(".nav-item").removeClass('active')
-					$(".nav-item").eq(index).addClass('active')
-
-					Relayout()
-
-				});
-			});
+			drawFull(startTime,endTime)
 
 
 		});
@@ -512,7 +533,6 @@ function getDone() {
 		dataTable.push(DataRow(function (i) { return (i * delayESP).toString() }, "Thời gian (ms)", stepPoint))
 		dataTable.push(DataRow(function (i) { return round(dataY1[i], 2) }, "Tọa độ (mm)", stepPoint))
 		createTable(dataTable, 'table');
-
 
 
 	});
